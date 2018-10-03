d7d13dd1d9d3947828d4a489e6b6b3a7cb5b66f5
Christopher Dyken <cdyk@users.noreply.github.com>
2018-10-03 17:35:11 +0200

    set up linking and initial window

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,27 @@
+#include <GLFW/glfw3.h>
+
 int main(int argc, char** argv)
 {
-	return 0;
+  GLFWwindow* window;
+
+  if (!glfwInit()) return -1;
+
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwTerminate();
+  return 0;
 }
